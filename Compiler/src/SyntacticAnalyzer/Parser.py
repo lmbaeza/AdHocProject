0af959500a28c5f38c9c8b70c4449d25e4c32ce1
jrbeltranl <jrbeltranl@unal.edu.co>
2019-11-29 22:05:12 -0500

    Se agregaron comentarios en detalle explicando la funcionalidad de cada segmento de código en los archivos Scanner.py Parser.py

--- Compiler/src/SyntacticAnalyzer/Parser.py
+++ Compiler/src/SyntacticAnalyzer/Parser.py
@@ -22,9 +22,10 @@ class PythonDataType:
 class Parser(object):
 
     def __init__(self):
-        # Tokens del Analizador Lexico
+        # Se obtienen los tokens del Analizador Lexico
         self.tokens = Lexer().getToken()
 
+        # Se define el nivel de prioridad para la asociación a la hora de reducir y/o desplazar las expresiones
         self.precedence = (
             ('left', 'PLUS', 'MINUS'),
             ('left', 'TIMES', 'DIVIDE'),
@@ -35,7 +36,7 @@ class Parser(object):
         self.parser = yacc.yacc(module=self)
 
 
-    # P_STATEMENT_ASSIGN
+    # P_STATEMENT_ASSIGN : Se define una lista de sentencias
 
     def p_statement_list(self, p):
         r'''statements-list : statement statements-list
@@ -47,7 +48,8 @@ class Parser(object):
         r'''statements-list : '''
 
 
-    # P_STATEMENT_FN_MAIN
+    # P_STATEMENT_FN_MAIN: Se establece la forma en que es definida la función main en el lenguaje:
+    # función(){ Lista de sentancias }
 
     def p_statement_fn_main(self, p):
         r'''statement : FN VOID MAIN LPAREN RPAREN LCURLY_BRACKET statements-list RCURLY_BRACKET'''
@@ -59,24 +61,28 @@ class Parser(object):
             next = tmp
 
 
-    # P_STATEMENT_IF
+    # P_STATEMENT_IF: Se establece la forma en que es definida un if en el lenguaje
+    # if(expression_generic){Lista de sentencias}
     def p_statement_if(self, p):
         r'''statement : IF LPAREN expression_generic RPAREN LCURLY_BRACKET statements-list RCURLY_BRACKET'''
         p[0] = StatementIf('If', p[3], p[6])
 
 
-    # P_STATEMENT_IF_ELSE
+    # P_STATEMENT_IF_ELSE: Se establece la forma en que es definido un if-else en el lenguaje
+    #  if(expression_generic){Lista de sentencias} else{lista de sentencias}
     def p_statement_if_else(self, p):
         r'''statement : IF LPAREN expression_generic RPAREN LCURLY_BRACKET statements-list RCURLY_BRACKET ELSE LCURLY_BRACKET statements-list RCURLY_BRACKET'''
         p[0] = StatementIfElse('IfElse', p[3], p[6], p[10])
     
-
+    # P_STATEMENT_ASSIGN_*: Se establece la fomra en que es definida una asignación en el lenguaje
+    # para enteros|flotantes|booleanos|cadenas
+    #  Identificador = expression_*; 
     def p_statement_assign_integer(self, p):
         r'''statement : INT ID EQUALS expression_integer SEMICOLON'''
 
         p[0] = StatementAssign('assign', 'INT', p, p[2], p[4])
 
-
+    
     def p_statement_assign_float(self, p):
         r'''statement : DOUBLE ID EQUALS expression_float SEMICOLON'''
         p[0] = StatementAssign('assign', 'DOUBLE', p, p[2], p[4])
@@ -86,14 +92,14 @@ class Parser(object):
         r'''statement : BOOLEAN ID EQUALS expression_boolean SEMICOLON'''
         p[0] = StatementAssign('assign', 'BOOLEAN', p, p[2], p[4])
 
-
+ 
     def p_statement_assign_string(self, p):
         r'''statement : STRING ID EQUALS expression_string SEMICOLON'''
         # p[0] = StatementAssign('assign', 'STRING', p, p[2], p[4])
 
 
-    # P_STATEMENT_EXPR
-
+    # P_STATEMENT_EXPR: Se define una expresión genérica:
+    # expression_generic;   | expression_generic
     def p_statement_expr_generic(self, p):
         r'''statement : expression_generic SEMICOLON
                     | expression_generic'''
@@ -101,15 +107,16 @@ class Parser(object):
         # StatementExpr(p, p[1])
 
 
-    # P_STATEMENT_UPDATE
-
+    # P_STATEMENT_UPDATE: Se define una sentencia de actualización genérica
+    # Identificador := expression_generic;
     def p_statement_update_generic(self, p):
         r'''statement : ID UPDATE expression_generic SEMICOLON'''
         p[0] = StatementUpdate('Update', p, p[1], p[3])
 
 
-    # P_EXPRESSION_BINOP
-
+    # P_EXPRESSION_BINOP: Se define una operación entre dos 
+    # enteros|flotantes
+    # expression_* (+|-|*|/) expression_*
     def p_expression_binop_integer(self, p):
         r'''expression_integer : expression_integer PLUS expression_integer
                             | expression_integer MINUS expression_integer
@@ -120,7 +127,7 @@ class Parser(object):
 
         p[0] = ExpressionBinop('BinOp', p, var,  p[1], p[3], p[2])
 
-
+    
     def p_expression_binop_float(self, p):
         r'''expression_float : expression_float PLUS expression_float
                     | expression_float MINUS expression_float
@@ -135,6 +142,9 @@ class Parser(object):
     # P_COMPARISON
     # https://stackoverflow.com/questions/47746590/python-ply-issue-with-if-else-and-while-statements
 
+    # P_COMPARISON_BINOP_INTEGER: Se define la comparación entre dos
+    # enteros|flotantes|booleanos|cadenas
+    # expression_* (==|!=|>|>=|<|<=) expression_*
     def p_comparison_binop_integer(self, p):
         r'''comparison : expression_integer EQUAL expression_integer
                         | expression_integer NOTEQ expression_integer
@@ -145,7 +155,7 @@ class Parser(object):
         var = 'comp'
         p[0] = ComparisonBinop('ComparisonBinOp', p, var, p[1], p[3], p[2])
     
-
+    
     def p_comparison_binop_float(self, p):
         r'''comparison : expression_float EQUAL expression_float
                         | expression_float NOTEQ expression_float
@@ -153,17 +163,16 @@ class Parser(object):
                         | expression_float LARGE_EQ expression_float
                         | expression_float SMALL expression_float
                         | expression_float SMALL_EQ expression_float'''
-
         # p[0] = ComparisonBinop(p, p[1], p[3], p[2])
     
-
+    
     def p_comparison_binop_boolean(self, p):
         r'''comparison : expression_boolean EQUAL expression_boolean
                         | expression_boolean NOTEQ expression_boolean'''
 
         # p[0] = ComparisonBinop(p, p[1], p[3], p[2])
     
-
+    
     def p_comparison_binop_string(self, p):
         r'''comparison : expression_string EQUAL expression_string
                         | expression_string NOTEQ expression_string
@@ -171,11 +180,10 @@ class Parser(object):
                         | expression_string LARGE_EQ expression_string
                         | expression_string SMALL expression_string
                         | expression_string SMALL_EQ expression_string'''
-
         # p[0] = ComparisonBinop(p, p[1], p[3], p[2])
 
-    # P_EXPRESSION_UMINUS
-
+    # P_EXPRESSION_UMINUS_INTEGER: Se establece la forma en que es definido un entero|flotante negativo
+    # -expression_*
     def p_expression_uminus_integer(self, p):
         r'''expression_integer : MINUS expression_integer %prec UMINUS'''
         p[0] = ExpressionUminus(p, p[2])
@@ -185,9 +193,9 @@ class Parser(object):
         r'''expression_float : MINUS expression_float %prec UMINUS'''
         p[0] = ExpressionUminus(p, p[2])
 
-
-    # P_EXPRESSION_GROUP
-
+    # P_EXPRESSION_GROUP_GENERIC: Se establece la forma en que se realizan las agrupaciones 
+    # genéricas|enteros|flotantes|booleanas|cadenas
+    # (expression_*)
     def p_expression_group_generic(self, p):
         '''expression_generic : LPAREN expression_generic RPAREN'''
         # p[0] = ExpressionGroup(p, p[2])
@@ -220,8 +228,8 @@ class Parser(object):
         # p[0] = p[2]
 
 
-    # P_EXPRESSION
-
+    # P_EXPRESSION: Se establece la forma en que se definen las expresiones
+    # genércias|enteros|flotantes|booleanas|cadenas
     def p_expression_integer_generic(self, p):
         r'''expression_generic : expression_integer
                                 | expression_float
@@ -250,8 +258,8 @@ class Parser(object):
         # p[0] = String(p[1])
 
 
-    # P_EXPRESSION_ID
-
+    # P_EXPRESSION_ID: Se establece la forma en que se definen los identificadores para
+    # genéricos|enteros|flotantes|booleanos|cadenas
     def p_expression_id_generic(self, p):
         r'''expression_generic : ID'''
         p[0] = ExpressionID(p, p[1])
@@ -277,22 +285,19 @@ class Parser(object):
         # p[0] = ExpressionID(p, p[1])
 
 
-    # P_EXPRESSION_COMPARISON
-
+    # P_EXPRESSION_COMPARISON: Se establece la forma en que se establece la comparación a booleano:
     def p_expression_comparison_to_boolean(self, p):
         r'''expression_boolean : comparison'''
         p[0] = p[1]
 
 
-    # P_PRINT_ALL_TYPES
-
+    # P_PRINT_ALL_TYPES: Se establece la forma de un print|println para cualquier tipo
+    # print(expression_generic);
     def p_print_all_type(self, p):
         r'''statement : PRINT LPAREN expression_generic RPAREN SEMICOLON'''
         
         # Print(p, p[3])
 
-
-    # P_PRINTLN_ALL_TYPES
 
     def p_println_all_type(self, p):
         r'''statement : PRINTLN LPAREN expression_generic RPAREN SEMICOLON
@@ -300,15 +305,13 @@ class Parser(object):
         # Println(p, p[3])
 
 
-    # P_EMPTY
-
+    # P_EMPTY: Se define el empty
     def p_empty(self, p):
         r'''statement : '''
         pass
 
 
-    # P_ERROR
-
+    # P_ERROR: Se define el error
     def p_error(self, p):
         print(p)
         ErrorNotMatch(p)
@@ -326,6 +329,6 @@ class Parser(object):
                 break
             self.parser.parse(s)
 
-
+    # RUN: Se corre el parser (Analizador sintáctico)
     def run(self, code):
         self.parser.parse(code)
