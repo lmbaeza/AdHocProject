729806bbba36b238d0b1536e7f1187fbabd1d59a
Luis Miguel Baez Aponte <lmbaeza@unal.edu.co>
2019-12-01 22:05:19 -0500

    Se agregó gran parte del transpilador del codigo intermedio a assembler

---
+++ Compiler/src/Assembler/AST.py
@@ -0,0 +1,253 @@
+
+class Expression(object):
+    def evaluate(self):
+        raise NotImplementedError
+
+countRAM = 0
+
+table = {}
+tmp = {}
+
+R1 = 'ax'
+R2 = 'dx'
+
+class StatementList(Expression):
+    def __init__(self, typeName, value, _next):
+        self.typeName = typeName
+        self.value = value
+        self.next = _next
+
+    def evaluate(self):
+        if self.value is not None:
+            data = self.value.evaluate()
+            if data:
+                print(data)
+        
+        return self.next
+
+class StatementAssignID(Expression):
+    def __init__(self, typeName, ID, op, value):
+        self.typeName = typeName
+        self.id = ID
+        self.op = op
+        self.value = value
+
+    def evaluate(self):
+        global countRAM
+        countRAM += 1
+        table[self.id] = countRAM
+
+        # print(self.value)
+
+        if isinstance(self.id, Expression):
+            self.id = self.id.evaluate()
+        if isinstance(self.op, Expression):
+            self.op = self.op.evaluate()
+        
+        if isinstance(self.value, ExpressionInteger):
+            self.value = self.value.evaluate()
+            return '\tCARGAR PTR[{addr}], {value}'.format(
+                addr=table.get(self.id), value=self.value
+            )
+        elif isinstance(self.value, Expression):
+            self.value = self.value.evaluate()
+
+
+        return '\tCARGAR PTR[{addr}], {r1}'.format(
+            addr=table.get(self.id), r1=R1
+        )
+
+
+class StatementAssignTMP(Expression):
+    def __init__(self, typeName, tmp, op, value):
+        self.typeName = typeName
+        self.tmp = tmp
+        self.op = op
+        self.value = value
+
+    def evaluate(self):
+        global countRAM
+        #countRAM += 1
+        
+        tmp[self.tmp] = {'r':R1, "op": self.value.evaluate()}
+
+        if isinstance(self.tmp, Expression):
+            self.tmp = self.tmp.evaluate()
+        if isinstance(self.op, Expression):
+            self.op = self.op.evaluate()
+        if isinstance(self.value, Expression):
+            self.value = self.value.evaluate()
+
+        
+
+class StatementExpr(Expression):
+    def __init__(self, typeName, expr):
+        self.typeName = typeName
+        self.expr = expr
+
+    def evaluate(self):
+        return '\tExpr'
+
+class ExpressionBinop(Expression):
+    def __init__(self, typeName, left, op, right):
+        self.typeName = typeName
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def evaluate(self):
+
+        if isinstance(self.left, ExpressionInteger) or\
+            isinstance(self.left, ExpressionFloat):
+            self.left = self.left.evaluate()
+        elif isinstance(self.left, ExpressionID):
+            number = tmp.get(self.left.evaluate())
+            number = number.get('r')
+            self.left = 'PTR['+number+']'
+        elif isinstance(self.left, ExpressionTMP):
+            number = tmp.get(self.left.evaluate())
+            number = number.get('r')
+            self.left = number
+        
+
+        if isinstance(self.op, Expression):
+            self.op = self.op.evaluate()
+        if isinstance(self.right, Expression):
+            self.right = self.right.evaluate()
+
+        print('\tCOPIAR {r1}, {value}'.format(
+            r1=R1,
+            value=str(self.left)
+        ))
+        print('\tCOPIAR {r2}, {value}'.format(
+            r2=R2,
+            value=str(self.right)
+        ))
+        # return str(self.left) + ' ' + str(self.op) + ' ' + str(self.right)
+
+        if self.op == '+':
+            return print('\tSUMA {r1}, {r2}'.format(r1=R1, r2=R2))
+        elif self.op == '-':
+            return print('\tRESTA {r1}, {r2}'.format(r1=R1, r2=R2))
+        elif self.op == '*':
+            return print('\tMULT {r1}, {r2}'.format(r1=R1, r2=R2))
+        elif self.op == '/':
+            return print('\tDIV {r1}, {r2}'.format(r1=R1, r2=R2))
+
+class ExpressionComparison(Expression):
+    def __init__(self, typeName, left, op, right):
+        self.typeName = typeName
+        self.left = left
+        self.op = op
+        self.right = right
+
+    def evaluate(self):
+
+        
+        if isinstance(self.left, ExpressionID):
+            self.left = self.left.evaluate()
+            
+            print('\tCOPIAR {r1}, PTR[{addr}]'.format(
+                r1=R1,
+                addr=table.get(self.left)
+            ))
+            
+        # if isinstance(self.op, Expression):
+        #     self.op = self.op.evaluate()
+        
+
+        if isinstance(self.right, ExpressionID):
+            self.right = self.right.evaluate()
+            print('\tCOPIAR {r2}, PTR[{addr}]'.format(
+                r2=R2,
+                addr=table.get(self.right)
+            ))
+        elif isinstance(self.right, ExpressionInteger):
+            self.right = self.right.evaluate()
+            print('\tCOPIAR {r2}, {num}'.format(
+                r2=R2,
+                num=self.right
+            ))
+        
+        return str(self.op)
+
+class ExpressionUminus(Expression):
+    def __init__(self, typeName, value):
+        self.typeName = typeName
+        self.value = value
+
+    def evaluate(self):
+        return str(-self.value)
+
+class ExpressionInteger(Expression):
+    def __init__(self, typeName, value):
+        self.typeName = typeName
+        self.value = value
+
+    def evaluate(self):
+        return str(self.value)
+
+class ExpressionFloat(Expression):
+    def __init__(self, typeName, value):
+        self.typeName = typeName
+        self.value = value
+
+    def evaluate(self):
+        return str(self.value)
+
+class ExpressionID(Expression):
+    def __init__(self, typeName, ID):
+        self.typeName = typeName
+        self.id = ID
+
+    def evaluate(self):
+        return str(self.id)
+
+class ExpressionTMP(Expression):
+    def __init__(self, typeName, tmp):
+        self.typeName = typeName
+        self.tmp = tmp
+
+    def evaluate(self):
+        return str(self.tmp)
+
+class ExpressionIFNOT(Expression):
+    def __init__(self, typeName, comparison, label):
+        self.typeName = typeName
+        self.comparison = comparison
+        self.label = label
+
+    def evaluate(self):
+        
+        out = '\tCOMPARAR {r1}, {r2}'.format(
+            r1=R1,
+            r2=R2
+        )
+
+        data = tmp.get(self.comparison)
+
+        if data.get('op'):
+            if data.get('op') == '==':
+                out += '\n\tSALTO_SI_UNO '+self.label
+            elif data.get('op') == '==':
+                out += '\n\SALTO_SI_CERO '+self.label
+        
+        return out
+        
+
+class ExpressionGOTO(Expression):
+    def __init__(self, typeName, value, label):
+        self.typeName = typeName
+        self.value = value
+        self.label = label
+
+    def evaluate(self):
+        return '\t'+str(self.value) + ' ' + self.label
+
+class ExpressionLABEL(Expression):
+    def __init__(self, typeName, value):
+        self.typeName = typeName
+        self.value = value
+
+    def evaluate(self):
+        return str(self.value)
--- Compiler/src/Assembler/ParserASM.py
+++ Compiler/src/Assembler/ParserASM.py
@@ -4,6 +4,7 @@
 from ply import yacc
 
 from Compiler.src.Assembler.LexASM import LexerASM
+from Compiler.src.Assembler.AST import *
 
 
 class ParserASM(object):
@@ -22,73 +23,89 @@ class ParserASM(object):
 
         self.lexer = LexerASM()
         self.parser = yacc.yacc(module=self)
+    
+    def p_run(self, p):
+        r'run : statements-list'
+
+        next = p[1]
+
+        while next is not None:
+            tmp = next.evaluate()
+            next = tmp
 
     def p_statement_list(self, p):
         r'''statements-list : statement statements-list'''
-        pass
+        p[0] = StatementList('Statement List', p[1], p[2])
+        # if p[1] is not None:
+            # print(p[1].evaluate())
+
     
     def p_statement_list_empty(self, p):
         r'''statements-list : '''
         pass
 
     
-    def p_statement_assign(self, p):
+    def p_statement_assign_id(self, p):
         r'statement : ID EQUALS expression'
-
-        print(list(p))
+        # print(list(p))
+        p[0] = StatementAssignID('ASSING_TMP', p[1], p[2], p[3])
 
     def p_statement_assign_tmp(self, p):
         r'statement : TMP EQUALS expression'
-
-        print(list(p))
+        # print(list(p))
+        p[0] = StatementAssignTMP('ASSING_TMP', p[1], p[2], p[3])
+        
 
     def p_statement_expr(self, p):
         r'statement : expression'
-        print(list(p))
+        p[0] = p[1]
     
     def p_expression_binop(self, p):
         '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression'''
-        print(list(p))
+        # print(list(p))
         
         if p[2] == '+':
-            p[0] = str(p[1]) + ' + '  + str(p[3])
+            p[0] = ExpressionBinop('BINOP', p[1], p[2], p[3])
         elif p[2] == '-':
-            p[0] = str(p[1]) + ' - '  + str(p[3])
+            p[0] = ExpressionBinop('BINOP', p[1], p[2], p[3])
         elif p[2] == '*':
-            p[0] = str(p[1]) + ' * '  + str(p[3])
+            p[0] = ExpressionBinop('BINOP', p[1], p[2], p[3])
         elif p[2] == '/':
-            p[0] = str(p[1]) + ' / '  + str(p[3])
+            p[0] = ExpressionBinop('BINOP', p[1], p[2], p[3])
     
     def p_expression_comparison(self, p):
         r'expression : expression EQUAL expression'
-        p[0] = str(p[1]) + ' == ' + str(p[3])
-        print(list(p))
+        p[0] = ExpressionComparison('COMPARISON', p[1], p[2], p[3])
 
     def p_expression_uminus(self, p):
         "expression : MINUS expression %prec UMINUS"
-        print(list(p))
-        p[0] = -p[2]
+        # print(list(p))
+        p[0] = ExpressionUminus('UMINUS', p[2])
+        # p[0] = -p[2]
     
 
     def p_expression_integer(self, p):
         "expression : INTEGER"
-        print(list(p))
-        p[0] = p[1]
+        # print(list(p))
+        p[0] = ExpressionInteger('INT', p[1])
+        # p[0] = p[1]
 
 
     def p_expression_float(self, p):
         "expression : FLOAT"
-        print(list(p))
-        p[0] = p[1]
+        # print(list(p))
+        p[0] = ExpressionFloat('FLOAT', p[1])
+        # p[0] = p[1]
 
 
-    def p_expression_name(self, p):
+    def p_expression_id(self, p):
         "expression : ID"
-        print(list(p))
-        p[0] = p[1]
+        # print(list(p))
+        p[0] = ExpressionID('ID', p[1])
+        # p[0] = p[1]
         # try:
         #     p[0] = names[p[1]]
         # except LookupError:
@@ -97,21 +114,27 @@ class ParserASM(object):
 
     def p_expression_tmp(self, p):
         "expression : TMP"
-        print(list(p))
-        p[0] = p[1]
+        # print(list(p))
+        p[0] = ExpressionTMP('TMP', p[1])
+        #p[0] = p[1]
 
     def p_expression_ifnot(self, p):
         r'''expression : IFNOT TMP GOTO LABEL
                        | IFNOT ID GOTO LABEL'''
-        print(list(p))
+        # print(list(p))
+        
+        p[0] = ExpressionIFNOT('IFNOT', p[2], p[4])
+        # print(list(p))
     
     def p_expression_goto(self, p):
         r'expression : GOTO LABEL'
-        print(list(p))
+        # print(list(p))
+        p[0] = ExpressionGOTO('GOTO', p[1], p[2])
 
     def p_expression_label(self, p):
         r'expression : LABEL'
-        print(list(p))
+        # print(list(p))
+        p[0] = ExpressionLABEL('LABEL', p[1])
 
 
     def p_error(self, p):
